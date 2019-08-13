b0fd168efeb6d9d5ec02f2aefa7364202c53418a
Boomika <bcedirisinghe@gmail.com>
2019-08-13 16:53:50 +0530

    Commit -8-13-2019

--- src/test/java/com/rezgateway/automation/tourmapper/AV_SC_ID18_TEST.java
+++ src/test/java/com/rezgateway/automation/tourmapper/AV_SC_ID18_TEST.java
@@ -130,22 +130,25 @@ public class AV_SC_ID18_TEST extends ExtentTestNGReportBuilderExt{
 
 	}
 
-	@Test(dependsOnMethods = "availbilityTest")
-	public synchronized void testHotelCode() {
-
-		ITestResult result = Reporter.getCurrentTestResult();
-		result.setAttribute("TestName", "Testing Hotel Code ");
-		result.setAttribute("Expected", "Hotel Code should be  : " + AviRequest.getCode()[0]);
-
-		if (AvailabilityResponse.getHotelList().containsKey(AviRequest.getCode()[0])) {
-			result.setAttribute("Actual", AvailabilityResponse.getHotelList().entrySet().iterator().next().getKey());
-		} else {
-			result.setAttribute("Actual", "User entered Hotel Code is not in the Response : Actual Hotel Code is " + AvailabilityResponse.getHotelList().entrySet().iterator().next().getKey());
-			Assert.fail("User entered Hotel Code is not in the Response:");
-		}
-
-	}
-
+	/*
+	 * @Test(dependsOnMethods = "availbilityTest") public synchronized void
+	 * testHotelCode() {
+	 * 
+	 * ITestResult result = Reporter.getCurrentTestResult();
+	 * result.setAttribute("TestName", "Testing Hotel Code ");
+	 * result.setAttribute("Expected", "Hotel Code should be  : " +
+	 * AviRequest.getCode()[0]);
+	 * 
+	 * if (AvailabilityResponse.getHotelList().containsKey(AviRequest.getCode()[0]))
+	 * { result.setAttribute("Actual",
+	 * AvailabilityResponse.getHotelList().entrySet().iterator().next().getKey()); }
+	 * else { result.setAttribute("Actual",
+	 * "User entered Hotel Code is not in the Response : Actual Hotel Code is " +
+	 * AvailabilityResponse.getHotelList().entrySet().iterator().next().getKey());
+	 * Assert.fail("User entered Hotel Code is not in the Response:"); }
+	 * 
+	 * }
+	 */
 	@Test(dependsOnMethods = "availbilityTest")
 	public synchronized void testDayWiseRateAvailability() {
 
